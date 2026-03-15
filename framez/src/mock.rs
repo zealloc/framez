9baab523c494acfefcba300f3fec436188f6a22c
Jad K. Haddad <jadkhaddad@gmail.com>
2026-03-15 12:18:02 +0100

    dep(embedded-io-async): updated embedded-io-async to version 0.7
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- framez/src/mock.rs
+++ framez/src/mock.rs
@@ -21,4 +21,8 @@ impl Write for Noop {
     async fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
         Ok(buf.len())
     }
+
+    async fn flush(&mut self) -> Result<(), Self::Error> {
+        Ok(())
+    }
 }
